3f7ddd256d1ab1e35bb307292874f34ed7213b31
afemath <anna@femath.com>
2015-05-12 11:00:18 -0400

    After the lesson updates

---
+++ step1/js/app.js
@@ -0,0 +1,28 @@
+var demoApp = angular.module('demoApp', [
+	'ngRoute',
+	'demoControllers'
+]);
+
+demoApp.run(function($rootScope) {
+    $rootScope.phones = null;
+});
+
+demoApp.config(['$routeProvider',
+	function($routeProvider) {
+		$routeProvider.
+		when('/phones', {
+			templateUrl: 'partials/phone-list.html',
+			controller: 'PhoneListCtrl'
+		}).
+		when('/phones/:phoneId', {
+			templateUrl: 'partials/phone-detail.html',
+			controller: 'PhoneDetailCtrl'
+		}).
+		when('/map', {
+			templateUrl: 'partials/map.html',
+			controller: 'MapCtrl'
+		}).
+		otherwise({
+		redirectTo: '/phones'
+	});
+}]);
